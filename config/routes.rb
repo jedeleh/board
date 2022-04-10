f02e0b2221e5affc92295c90325eb44dfb3e1942
JD Kaplan <jedeleh@users.noreply.github.com>
2022-04-10 13:34:24 -0400

    Models pt 2 (#2)
    * Scaffold for post
    
    * Comment model in place. still need limited controller
    
    * the controller and limited views/action
    
    * adding an action to run tests on branches

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'comments/create'
+  get 'comments/update'
+  get 'comments/show'
+  resources :posts
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
