7832628d45708f8bdae0d1f5b697802886ca2959
Jedeleh <jaydel@gmail.com>
2022-04-14 08:58:58 -0400

    Kinda need to turn this in--even though it's incomplete, hopefully it gives enough insight into my thought process, etc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'comments/create'
-  get 'comments/update'
+  post 'comments/create'
+  post 'comments/update'
   get 'comments/show'
+
+  post 'posts/create'
+  get 'posts/show'
+
   resources :posts
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
